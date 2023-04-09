d6c1c7803de4719f80c19c94e524718eec5a7fb0
Hana <a.hana@aui.ma>
2023-04-09 14:40:07 +0000

    plz :')

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -2,6 +2,6 @@ import Bull from "bull";
 import { env } from "~/env.mjs";
 
 // eslint-disable-next-line
-const processQueue = new Bull("process", env.EDIS_URL);
+const processQueue = new Bull("prc", env.EDIS_URL);
 console.log(env.EDIS_URL);
 export default processQueue;
