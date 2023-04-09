40a4279b880fc44ac31fbd4781528cfb82fe7112
Hana <a.hana@aui.ma>
2023-04-09 14:43:38 +0000

    silliest mistake

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -2,6 +2,8 @@ import Bull from "bull";
 import { env } from "~/env.mjs";
 
 // eslint-disable-next-line
-const processQueue = new Bull("prc", env.EDIS_URL);
+const processQueue = new Bull("prc", {
+    redis: env.EDIS_URL,
+});
 console.log(env.EDIS_URL);
 export default processQueue;
