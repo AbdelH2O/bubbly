e73e0562d903659ca1604e876701b563e47df6c5
Hana <a.hana@aui.ma>
2023-04-09 16:18:13 +0000

    please ser

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -1,8 +1,8 @@
 import Bull from "bull";
 import { env } from "~/env.mjs";
 
-const processQueue = new Bull("prc", {
-    redis: env.EDIS_URL,
+const processQueue = new Bull("prc", env.EDIS_URL, {
+    // redis: env.EDIS_URL,
 });
 console.log(env.EDIS_URL);
 export default processQueue;
